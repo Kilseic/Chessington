c53e515c95f9b733a24845f5111082400d8c97f8
Leon Cieslik <Leon.Cieslik@softwire.com>
2020-07-13 10:45:19 +0100

    Pawn now has avaiable moves

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,21 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square location = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+            if (Player == Player.White)
+            {
+                Square move = new Square(location.Row-1,location.Col);
+                availableMoves.Add(move);
+            }
+            else
+            {
+                Square move = new Square(location.Row+1,location.Col);
+                availableMoves.Add(move);
+            }
+
+            IEnumerable<Square> listOfMoves = availableMoves;
+            return listOfMoves;
         }
     }
 }
