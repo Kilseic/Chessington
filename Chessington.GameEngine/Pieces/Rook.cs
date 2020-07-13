1fce737dc817ee2b4b6f56cc7dd6244d4c9b106d
Leon Cieslik <Leon.Cieslik@softwire.com>
2020-07-13 11:25:33 +0100

    Rook now has avaiable moves

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,18 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square location = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+            for (var i = 0; i < 8; i++)
+            {
+                availableMoves.Add(Square.At(location.Row, i));
+                availableMoves.Add(Square.At(i, location.Col));
+            }
+
+            //Get rid of our starting location.
+            availableMoves.RemoveAll(s => s == Square.At(location.Row, location.Col));
+            IEnumerable<Square> listOfMoves = availableMoves;
+            return listOfMoves;
         }
     }
 }
