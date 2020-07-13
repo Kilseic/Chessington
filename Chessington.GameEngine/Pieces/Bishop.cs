3b051fc976dce37c86b48501110e3c71ac1d0415
Leon Cieslik <Leon.Cieslik@softwire.com>
2020-07-13 12:33:30 +0100

    Bishop now has available moves

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,39 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> availableMoves = new List<Square>();
+            Square location = board.FindPiece(this);
+            for (int i = 1; i < 8; i++)
+            {
+                IEnumerable<int> onBoardCheck = Enumerable.Range(0, 8);
+                if (onBoardCheck.Contains(location.Row + i))
+                {
+                    if (onBoardCheck.Contains(location.Col + i))
+                    {
+                        availableMoves.Add(Square.At(location.Row+i,location.Col+i));
+                    }
+
+                    if (onBoardCheck.Contains(location.Col - i))
+                    {
+                        availableMoves.Add(Square.At(location.Row+i,location.Col-i));
+                    }
+                }
+                if (onBoardCheck.Contains(location.Row - i))
+                {
+                    if (onBoardCheck.Contains(location.Col - i))
+                    {
+                        availableMoves.Add(Square.At(location.Row-i,location.Col-i));
+                    }
+
+                    if (onBoardCheck.Contains(location.Col + i))
+                    {
+                        availableMoves.Add(Square.At(location.Row-i,location.Col+i));
+                    }
+                }
+            }
+
+            IEnumerable<Square> listOfMoves = availableMoves;
+            return listOfMoves;
         }
     }
 }
