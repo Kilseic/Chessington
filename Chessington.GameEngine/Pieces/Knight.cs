75d0e4c4a4cca167787d2bd1d24f10dfb258cf38
Leon Cieslik <Leon.Cieslik@softwire.com>
2020-07-14 14:10:50 +0100

    knights now have proper moveset

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -12,23 +12,38 @@ namespace Chessington.GameEngine.Pieces
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             Square location = board.FindPiece(this);
-            List<Square> availableMoves = AddKnightMoves(location).ToList();
+            List<Square> availableMoves = AddKnightMoves(location, board).ToList();
             var outputMoves = availableMoves.Where(Board.InRange);
             return outputMoves;
         }
 
-        private List<Square> AddKnightMoves(Square location)
+        private List<Square> AddKnightMoves(Square location, Board board)
         {
             List<Square> moveOutput = new List<Square>();
-            moveOutput.Add(Square.At(location.Row+2, location.Col+1));
-            moveOutput.Add(Square.At(location.Row+2, location.Col-1));
-            moveOutput.Add(Square.At(location.Row-2, location.Col+1));
-            moveOutput.Add(Square.At(location.Row-2, location.Col-1));
-            moveOutput.Add(Square.At(location.Row+1, location.Col+2));
-            moveOutput.Add(Square.At(location.Row+1, location.Col-2));
-            moveOutput.Add(Square.At(location.Row-1, location.Col+2));
-            moveOutput.Add(Square.At(location.Row-1, location.Col-2));
+            TryAddMove(Square.At(location.Row + 2, location.Col + 1));
+            TryAddMove(Square.At(location.Row+2, location.Col-1));
+            TryAddMove(Square.At(location.Row-2, location.Col+1));
+            TryAddMove(Square.At(location.Row-2, location.Col-1));
+            TryAddMove(Square.At(location.Row+1, location.Col+2));
+            TryAddMove(Square.At(location.Row+1, location.Col-2));
+            TryAddMove(Square.At(location.Row-1, location.Col+2));
+            TryAddMove(Square.At(location.Row-1, location.Col-2));
             return moveOutput;
+
+            void TryAddMove(Square inputSquare)
+            {
+                if (Board.InRange(inputSquare))
+                {
+                    if (Board.SquareOpen(board, inputSquare))
+                    {
+                        moveOutput.Add(inputSquare);
+                    }
+                    else if (Board.SquareLoyalty(board, inputSquare) != Player)
+                    {
+                        moveOutput.Add(inputSquare);
+                    }
+                }
+            }
         }
     }
 }
