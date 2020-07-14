7f8976f009d5553a3098a7c41fd2e752d008ce65
Leon Cieslik <Leon.Cieslik@softwire.com>
2020-07-14 14:04:04 +0100

    pawns now can't move off board

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -14,31 +15,43 @@ namespace Chessington.GameEngine.Pieces
             List<Square> availableMoves = new List<Square>();
             if (Player == Player.White)
             {
-                if (Board.SquareOpen(board,Square.At(location.Row - 1, location.Col)))
-                {
-                    availableMoves.Add(Square.At(location.Row - 1, location.Col));
-                    if (HasEverMoved == false)
-                    {
-                        if (Board.SquareOpen(board, Square.At(location.Row - 2, location.Col)))
-                            availableMoves.Add(Square.At(location.Row - 2, location.Col));
-                    }
-                }
+                TryAddMove(location.Row - 1, location.Row - 2);
             }
             else
             {
-                if (Board.SquareOpen(board,Square.At(location.Row + 1, location.Col)))
+                TryAddMove(location.Row + 1, location.Row + 2);
+            }
+            IEnumerable<Square> listOfMoves = availableMoves;
+            return listOfMoves;
+
+            void TryAddMove(int row1, int row2)
+            {
+                Square frontByOne = Square.At(row1, location.Col);
+                if (CheckSquareMove(frontByOne))
                 {
-                    availableMoves.Add(Square.At(location.Row+1,location.Col));
-                    if (HasEverMoved == false)
+                    availableMoves.Add(frontByOne);
+                }
+                if ((HasEverMoved == false) & CheckSquareMove(frontByOne))
+                {
+                    Square frontByTwo = Square.At(row2, location.Col);
+                    if (CheckSquareMove(frontByTwo))
                     {
-                        if (Board.SquareOpen(board,Square.At(location.Row+2,location.Col))) 
-                            availableMoves.Add(Square.At(location.Row+2,location.Col));
-                    }
+                        availableMoves.Add(frontByTwo);
+                    };
                 }
             }
 
-            IEnumerable<Square> listOfMoves = availableMoves;
-            return listOfMoves;
+            bool CheckSquareMove(Square inputSquare)
+            {
+                if (Board.InRange(inputSquare))
+                {
+                    if (Board.SquareOpen(board,inputSquare))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
         }
     }
 }
